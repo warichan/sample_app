210c53112e5aea0acd01d1646aa837f1419984d1
warichan <txaxmxmxm@gmail.com>
2016-04-21 22:56:44 +0900

    update add reset to users

--- db/migrate/20160421031218_add_reset_to_users.rb
+++ db/migrate/20160421031218_add_reset_to_users.rb
@@ -1,6 +1,6 @@
 class AddResetToUsers < ActiveRecord::Migration
   def change
     add_column :users, :reset_digest, :string
-    add_column :users, :reset_send_at, :detetime
+    add_column :users, :reset_send_at, :datetime
   end
 end
