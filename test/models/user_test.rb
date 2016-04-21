78d549e91a20987ec47109113a9a94a5cd4c4d1d
warichan <txaxmxmxm@gmail.com>
2016-04-21 11:38:39 +0900

    Add account activations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,11 +22,15 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "email validation should reject invalid addresses" do
-  invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                          foo@bar_baz.com foo@bar+baz.com]
-  invalid_addresses.each do |invalid_address|
-    @user.email = invalid_address
-    assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
- end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
 end
